c8fc044689128bcd7fe26a65f1c14b6c46a1c8ed
Hossam-Sayed <hossamsayed3030@gmail.com>
2025-06-11 16:36:00 +0300

    Give beginnerRecommender bean precedence using name

--- src/main/java/com/example/course_recommender/service/CoursesService.java
+++ src/main/java/com/example/course_recommender/service/CoursesService.java
@@ -2,23 +2,21 @@ package com.example.course_recommender.service;
 
 import com.example.course_recommender.model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CoursesService {
-    private final CourseRecommender courseRecommender;
+    private final CourseRecommender beginnerRecommender;
 
     @Autowired
-    // Use @Qualifier to specify which bean to inject based on its name
-    public CoursesService(@Qualifier("advancedRecommender") CourseRecommender courseRecommender) {
-        this.courseRecommender = courseRecommender;
-        System.out.println("CoursesService initialized with @Qualifier: " + courseRecommender.getClass().getSimpleName());
+    public CoursesService(CourseRecommender beginnerRecommender) {
+        this.beginnerRecommender = beginnerRecommender;
+        System.out.println("CoursesService initialized by name: " + beginnerRecommender.getClass().getSimpleName());
     }
 
     public List<Course> getRecommendedCourses() {
-        return courseRecommender.recommendedCourses();
+        return beginnerRecommender.recommendedCourses();
     }
 }
