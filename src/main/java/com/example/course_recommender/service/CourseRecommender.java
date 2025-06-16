29af59cc5a6d4a46b690ca8b3f9682fee63aa7a0
Hossam-Sayed <hossamsayed3030@gmail.com>
2025-06-16 14:06:36 +0300

    Restructure project

--- src/main/java/com/example/course_recommender/service/CourseRecommender.java
+++
@@ -1,9 +0,0 @@
-package com.example.course_recommender.service;
-
-import com.example.course_recommender.model.Course;
-
-import java.util.List;
-
-public interface CourseRecommender {
-    List<Course> recommendedCourses();
-}
