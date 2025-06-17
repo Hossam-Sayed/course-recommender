665204d58a121f86dfa6ed101055ff7726b4b111
Hossam-Sayed <hossamsayed3030@gmail.com>
2025-06-17 16:12:34 +0300

    Add course recommender implementation

---
+++ src/main/java/com/example/course_recommender/service/CourseRecommenderImpl.java
@@ -0,0 +1,42 @@
+package com.example.course_recommender.service;
+
+import com.example.course_recommender.model.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * A placeholder implementation of the CourseRecommender interface.
+ * For now, it simply returns all courses available in the system.
+ * This can be extended later with actual recommendation algorithms.
+ */
+@Service // Marks this class as a Spring service component
+public class CourseRecommenderImpl implements CourseRecommender {
+
+    private final CourseService courseService;
+
+    /**
+     * Constructor for CourseRecommenderImpl. Spring will automatically
+     * inject the CourseService instance.
+     *
+     * @param courseService The CourseService instance.
+     */
+    @Autowired
+    public CourseRecommenderImpl(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    /**
+     * Provides a list of recommended courses.
+     * Currently, this method returns all available courses.
+     *
+     * @return A list of all Course objects.
+     */
+    @Override
+    public List<Course> recommendedCourses() {
+        System.out.println("Executing recommendedCourses: returning all available courses.");
+        // TODO: Enhance recommendation logic
+        return courseService.getAllCourses();
+    }
+}
